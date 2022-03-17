e5f68334fe267b03c1c48bec1864325a8f0e5087
joaogcfa <joaogcfa@al.insper.edu.br>
2022-03-17 15:33:59 -0300

    matrix de path

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@
 using namespace std;
 typedef vector<vector<int>> matrix;
 
+
 matrix Smith(string a, string b, int n, int m, matrix H);
+
+struct path
+{
+  int direction;
+};
+
+typedef vector<vector<path>> matrixpath;
+
 
 void show_matrix(vector<vector<int>> H, int n, int m){
   cout << "" << endl;
@@ -21,9 +30,21 @@ void show_matrix(vector<vector<int>> H, int n, int m){
   cout << "" << endl;
 }
 
+void show_matrix_path(vector<vector<path>> H, int n, int m){
+  cout << "" << endl;
+  for(int i = 0; i < n; i++){
+    cout << " " << endl;
+    for(int j = 0; j < m; j++){
+      cout << H[i][j].direction << " ";
+    }
+  }
+  cout << "" << endl;
+  cout << "" << endl;
+}
 
 
-matrix Smith(string a, string b, int n, int m, matrix H){
+
+matrix Smith(string a, string b, int n, int m, matrix H, matrixpath path){
   int diagonal, deletion, insertion, w;
 
     for(int i = 1; i<=n; i++){
@@ -36,29 +57,63 @@ matrix Smith(string a, string b, int n, int m, matrix H){
             w=-1;
           }
 
-          // cout << w << endl;
-          // cout << H[i-1][j-1]<< endl;
 
           diagonal = H[i-1][j-1] + w;
-          // cout << diagonal;
           deletion = H[i-1][j] -1;
           insertion = H[i][j-1] -1;
 
-          H[i][j] = max({0,diagonal,deletion,insertion});
-            
+          H[i][j] = max({0,diagonal,deletion,insertion});  
         }
     }
     
     return H;
 }
 
+matrixpath Smithpath(string a, string b, int n, int m, matrix H, matrixpath path){
+  int diagonal, deletion, insertion, w;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
+
+          if (a[i] == b[j]){
+            w = 2;
+          }
+          else{
+            w=-1;
+          }
+
+
+          diagonal = H[i-1][j-1] + w;
+          deletion = H[i-1][j] -1;
+          insertion = H[i][j-1] -1;
+
+          H[i][j] = max({0,diagonal,deletion,insertion});
+          if(max({0,diagonal,deletion,insertion}) == diagonal){
+            path[i][j].direction = 1;//"diagonal";
+          }
+          if(max({0,diagonal,deletion,insertion}) == deletion){
+            path[i][j].direction = 2;//"deletion";
+          }
+          if(max({0,diagonal,deletion,insertion}) == insertion){
+            path[i][j].direction = 3;//"insertion";
+          }
+            
+        }
+    }
+    
+    return path;
+}
+
+
+
 
 int main(){
     int n,m;
-    // float w;
-    string seq1,seq2;
-    matrix H;
     char first_signal = '-';
+    string seq1,seq2;
+    matrix H, H2;
+    matrixpath path;
+    
 
 
     cin >> n;
@@ -72,13 +127,37 @@ int main(){
     
 
     H.resize(n+1);
+    H2.resize(n+1);
+    path.resize(n+1);
     for(int e=0; e<=n; e++){
         H[e].resize(m+1);
+        H2[e].resize(m+1);
+        path[e].resize(m+1);
     }
 
-    H = Smith(seq1,seq2,n,m,H);
+    H = Smith(seq1,seq2,n,m,H,path);
+    path = Smithpath(seq1,seq2,n,m,H,path);
 
+    cout << "Matrix after algorithm: ";
     show_matrix(H,n,m);
+    show_matrix_path(path,n,m);
+
+
+    int top_H = 0;
+    int top_j = 0;
+    int top_i = 0;
+
+    for (int i = 1; i<n; i++){
+      for(int j = 1; j<m; j++){
+        if(H[i][j] > top_H){
+          top_H = H[i][j];
+          top_i = i;
+          top_j = j;
+        }
+      }
+    }
+
+    cout << "Highest value in H: " << top_H << endl;
 
 
     return 0;
