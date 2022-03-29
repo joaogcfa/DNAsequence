a314eb013d32a7adf8681a64e1740fccd5f4c7f4
joaogcfa <joaogcfa@al.insper.edu.br>
2022-03-29 14:36:24 -0300

    SubSequencia

--- main.cpp
+++ main.cpp
@@ -2,186 +2,60 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <random>
 
 using namespace std;
 
-struct path
-{
-  string direction;
-};
-struct retVals
-{
-  string str1, str2;
-};
-
-typedef vector<vector<int>> matrix;
-typedef vector<vector<path>> matrixpath;
-
-matrix Smith(string a, string b, int n, int m, matrix H)
-{
-  int diagonal, deletion, insertion, w;
-
-  for (int i = 1; i <= n; i++)
-  {
-    for (int j = 1; j <= m; j++)
-    {
-
-      if (a[i] == b[j])
-      {
-        w = 2;
-      }
-      else
-      {
-        w = -1;
-      }
-
-      diagonal = H[i - 1][j - 1] + w;
-      deletion = H[i - 1][j] - 1;
-      insertion = H[i][j - 1] - 1;
-
-      H[i][j] = max({0, diagonal, deletion, insertion});
-    }
-  }
-
-  return H;
-}
-
-matrixpath Smithpath(string a, string b, int n, int m, matrix H, matrixpath path)
-{
-  int diagonal, deletion, insertion, w;
-
-  for (int i = 1; i <= n; i++)
-  {
-    for (int j = 1; j <= m; j++)
-    {
-
-      if (a[i] == b[j])
-      {
-        w = 2;
-      }
-      else
-      {
-        w = -1;
-      }
-
-      diagonal = H[i - 1][j - 1] + w;
-      deletion = H[i - 1][j] - 1;
-      insertion = H[i][j - 1] - 1;
-
-      H[i][j] = max({0, diagonal, deletion, insertion});
-      if (max({0, diagonal, deletion, insertion}) == diagonal)
-      {
-        path[i][j].direction = "diagonal";
-      }
-      if (max({0, diagonal, deletion, insertion}) == deletion)
-      {
-        path[i][j].direction = "delation";
-      }
-      if (max({0, diagonal, deletion, insertion}) == insertion)
-      {
-        path[i][j].direction = "insertion";
-      }
-    }
-  }
-
-  return path;
-}
-
-retVals climbing(string seq1, string seq2, string combination1, string combination2, int i, int j, matrix H, matrixpath path)
-{
-  int order = 0;
-
-  while ((i != 0 || j != 0) && H[i][j] != 0)
-  {
-
-    // cout << path[i][j].direction;
-    if (path[i][j].direction == "insertion")
-    {
-
-      combination1 += '-';
-      combination2 += seq2[j];
-      j--;
-    }
-    else if (path[i][j].direction == "delation")
-    {
-      combination1 += seq1[i];
-      combination2 += '-';
-      i--;
-    }
-    else if (path[i][j].direction == "diagonal")
-    {
-      combination1 += seq1[i];
-      combination2 += seq2[j];
-      i--;
-      j--;
-    }
-    order++;
-  }
-  return retVals{combination1, combination2};
-}
-
 int main()
 {
-  int n, m;
-  char first_signal = '-';
-  string seq1, seq2;
-  matrix H, H2;
-  matrixpath path;
+  int n, m, k, j, i, p;
+  // int j = 0;
+  string seq1, seq2, sb1, sb2;
+
+  std::default_random_engine generator;
+  // generator.seed(10);
 
   cin >> n;
   cin >> m;
-  n++; // Adicionando caracter especial
-  m++; // Adicionando caracter especial
 
   cin >> seq1;
   cin >> seq2;
 
-  seq1 = first_signal + seq1;
-  seq2 = first_signal + seq2;
-
-  H.resize(n + 1);
-  H2.resize(n + 1);
-  path.resize(n + 1);
-  for (int e = 0; e <= n; e++)
+  if (n > m)
   {
-    H[e].resize(m + 1);
-    H2[e].resize(m + 1);
-    path[e].resize(m + 1);
+    std::uniform_int_distribution<int> distribution_k(1, m);
+    k = distribution_k(generator);
   }
-
-  H = Smith(seq1, seq2, n, m, H);
-  path = Smithpath(seq1, seq2, n, m, H, path);
-
-  int top_H = 0;
-  int top_j = 0;
-  int top_i = 0;
-
-  // Pegando maior valor
-  for (int i = 1; i < n; i++)
+  else
   {
-    for (int j = 1; j < m; j++)
+    std::uniform_int_distribution<int> distribution_k(1, n);
+    k = distribution_k(generator);
+    while (k < 2)
     {
-      if (H[i][j] > top_H)
-      {
-        top_H = H[i][j];
-        top_i = i;
-        top_j = j;
-      }
+      k = distribution_k(generator);
     }
   }
 
-  string comb1;
-  string comb2;
+  std::uniform_int_distribution<int> distribution_j(0, m - k);
+  j = distribution_j(generator);
 
-  comb1.reserve(n + 1);
-  comb2.reserve(m + 1);
+  // std::uniform_int_distribution<int> distribution_i(0, n - k);
+  // i = distribution_i(generator);
 
-  retVals palavra = climbing(seq1, seq2, comb1, comb2, top_i, top_j, H, path);
+  cout << "k: " << k << " j: " << j << endl;
 
-  reverse(palavra.str1.begin(), palavra.str1.end());
-  reverse(palavra.str2.begin(), palavra.str2.end());
-  cout << "comb1: " << palavra.str1 << endl
-       << "comb2: " << palavra.str2 << endl;
+  sb2 = seq2.substr(j, k);
+
+  cout << "sb2: " << sb2;
+
+  std::uniform_int_distribution<int> distribution_p(2, n + m);
+  p = distribution_p(generator);
+
+  while (p > 0 && i > n - k)
+  {
+    sb1 = seq1.substr(i, k);
+    p--;
+  }
 
   return 0;
 }
